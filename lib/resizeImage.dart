7c26c7ceb04f2cd26e12ed22d988353a45088996
St-Luciferr <santosh.762417@thc.tu.edu.np>
2023-02-05 01:28:23 +0545

    resize function added for captured image

---
+++ lib/resizeImage.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:flutter_native_image/flutter_native_image.dart';
+
+Future<XFile> resizeImage(img) async {
+  ImageProperties properties =
+      await FlutterNativeImage.getImageProperties(img.path);
+
+  File compressedFile = await FlutterNativeImage.compressImage(img.path,
+      quality: 90, targetWidth: 300, targetHeight: 300);
+
+  // delete original file
+  try {
+    if (await img.exists()) {
+      await img.delete();
+    }
+  } catch (e) {
+    // Error in getting access to the file.
+  }
+
+  return XFile(compressedFile.path);
+}
