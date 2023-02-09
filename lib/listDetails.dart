f0e32672f744371939c9525a1a6d706aef49d93b
St-Luciferr <santosh.762417@thc.tu.edu.np>
2023-02-10 02:29:26 +0545

    List views for detedted moument initial

--- lib/listDetails.dart
+++ lib/listDetails.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 class ListViews extends StatefulWidget {
-  const ListViews({super.key, required this.monuments});
-  final List<String> monuments;
+  ListViews({super.key, required this.monuments});
+  final List<dynamic> monuments;
   @override
   State<ListViews> createState() => _ListViewsState();
 }
@@ -15,68 +15,99 @@ class _ListViewsState extends State<ListViews> {
       appBar: AppBar(
         title: const Text('Detected Monuments'),
       ),
-      body: ListView(padding: EdgeInsets.all(10), children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Align(
-                alignment: Alignment.topLeft,
-                child: Image(
-                    image: AssetImage('assets/img.jpg'),
-                    height: 150,
-                    width: 150)),
-            Text('name: Khatra Monument', textAlign: TextAlign.left),
-            Text('Date of Construction: 2056/01/24', textAlign: TextAlign.left),
-            Text('Constructed By: Santosh Pandey', textAlign: TextAlign.left),
-            Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
-                'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-                'Cursus vitae congue mauris rhoncus. Lectus proin nibh nisl condimentum',
-                textAlign: TextAlign.justify),
-          ],
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Align(
-                alignment: Alignment.topLeft,
-                child: Image(
-                    image: AssetImage('assets/img.jpg'),
-                    height: 150,
-                    width: 150)),
-            Text('name: Khatra Monument', textAlign: TextAlign.left),
-            Text('Date of Construction: 2056/01/24', textAlign: TextAlign.left),
-            Text('Constructed By: Santosh Pandey', textAlign: TextAlign.left),
-            Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
-                'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-                'Cursus vitae congue mauris rhoncus. Lectus proin nibh nisl condimentum',
-                textAlign: TextAlign.justify),
-          ],
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Align(
-                alignment: Alignment.topLeft,
-                child: Image(
-                    image: AssetImage('assets/img.jpg'),
-                    height: 150,
-                    width: 150)),
-            Text('name: Khatra Monument', textAlign: TextAlign.left),
-            Text('Date of Construction: 2056/01/24', textAlign: TextAlign.left),
-            Text('Constructed By: Santosh Pandey', textAlign: TextAlign.left),
-            Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
-                'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-                'Cursus vitae congue mauris rhoncus. Lectus proin nibh nisl condimentum',
-                textAlign: TextAlign.justify),
-          ],
-        ),
-      ]),
+      body: ListView(
+        padding: EdgeInsets.all(10),
+        children: <Widget>[
+          for (var monument in widget.monuments)
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Image(
+                      image: AssetImage('assets/img.jpg'),
+                      height: 150,
+                      width: 150),
+                ),
+                Text(
+                    "Detected Monument: ${monument['detectedClass']} \nConfidence Score: ${monument['confidenceInClass']}",
+                    textAlign: TextAlign.left),
+                Text('Date of Construction: 2056/01/24',
+                    textAlign: TextAlign.left),
+                Text('Constructed By: Santosh Pandey',
+                    textAlign: TextAlign.left),
+                Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
+                    'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                    'Cursus vitae congue mauris rhoncus. Lectus proin nibh nisl condimentum',
+                    textAlign: TextAlign.justify),
+              ],
+            ),
+        ],
+
+        // [
+        //   Column(
+        //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //     crossAxisAlignment: CrossAxisAlignment.start,
+        //     children: [
+        //       Align(
+        //           alignment: Alignment.topLeft,
+        //           child: Image(
+        //               image: AssetImage('assets/img.jpg'),
+        //               height: 150,
+        //               width: 150)),
+        //       Text('name: Khatra Monument', textAlign: TextAlign.left),
+        //       Text('Date of Construction: 2056/01/24', textAlign: TextAlign.left),
+        //       Text('Constructed By: Santosh Pandey', textAlign: TextAlign.left),
+        //       Text(
+        //           'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
+        //           'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        //           'Cursus vitae congue mauris rhoncus. Lectus proin nibh nisl condimentum',
+        //           textAlign: TextAlign.justify),
+        //     ],
+        //   ),
+        //   Column(
+        //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //     crossAxisAlignment: CrossAxisAlignment.start,
+        //     children: [
+        //       Align(
+        //           alignment: Alignment.topLeft,
+        //           child: Image(
+        //               image: AssetImage('assets/img.jpg'),
+        //               height: 150,
+        //               width: 150)),
+        //       Text('name: Khatra Monument', textAlign: TextAlign.left),
+        //       Text('Date of Construction: 2056/01/24', textAlign: TextAlign.left),
+        //       Text('Constructed By: Santosh Pandey', textAlign: TextAlign.left),
+        //       Text(
+        //           'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
+        //           'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        //           'Cursus vitae congue mauris rhoncus. Lectus proin nibh nisl condimentum',
+        //           textAlign: TextAlign.justify),
+        //     ],
+        //   ),
+        //   Column(
+        //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //     crossAxisAlignment: CrossAxisAlignment.start,
+        //     children: [
+        //       Align(
+        //           alignment: Alignment.topLeft,
+        //           child: Image(
+        //               image: AssetImage('assets/img.jpg'),
+        //               height: 150,
+        //               width: 150)),
+        //       Text('name: Khatra Monument', textAlign: TextAlign.left),
+        //       Text('Date of Construction: 2056/01/24', textAlign: TextAlign.left),
+        //       Text('Constructed By: Santosh Pandey', textAlign: TextAlign.left),
+        //       Text(
+        //           'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
+        //           'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        //           'Cursus vitae congue mauris rhoncus. Lectus proin nibh nisl condimentum',
+        //           textAlign: TextAlign.justify),
+        //     ],
+        //   ),
+      ),
     );
   }
 }
